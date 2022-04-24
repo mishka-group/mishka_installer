8ba5a5a37e9a3f93e194cdd5d719945b03e17ba1
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-04-24 12:40:57 +0430

    add functions {compare_dependencies, deps, prepend_compiled_apps, get_build_path}

--- lib/installer/run_time_sourcing.ex
+++ lib/installer/run_time_sourcing.ex
@@ -1,13 +1,44 @@
 defmodule MishkaInstaller.Installer.RunTimeSourcing do
-  def get_installed_libs(_app) do
+  def get_installed_apps() do
 
   end
 
-  def compiled_libs(_mode \\ Mix.env() || :dev) do
+  def compiled_apps(_mode \\ Mix.env()) do
 
   end
 
-  def compare_dependencies(_installed_libs, _new_compiled) do
+  def compare_dependencies(installed_apps \\ Application.loaded_applications, files_list) do
+    installed_apps = Map.new(installed_apps, fn {app, _des, _ver} = item -> {Atom.to_string(app), item} end)
+    Enum.map(files_list, fn app_name ->
+      case Map.fetch(installed_apps, app_name) do
+        :error ->
+          app_name
+        _ ->
+          nil
+        end
+    end)
+    |> Enum.reject(& is_nil(&1))
+  end
 
+  def deps() do
+    [{:get, "deps.get"}, {:compile, "deps.compile"}]
+    |> Enum.map(fn {operation, command} ->
+      {stream, status} = System.cmd("mix", [command], into: IO.stream())
+      %{operation: operation, output: stream, status: status}
+    end)
+  end
+
+  def prepend_compiled_apps(files_list) do
+    files_list
+    |> Enum.map(& {String.to_atom(&1), Path.join(get_build_path() <> "/" <> &1, "ebin")  |> Code.prepend_path})
+    |> Enum.filter(fn {_app, status} -> status == {:error, :bad_directory} end)
+    |> case do
+      [] -> {:ok, :prepend_compiled_apps}
+      list -> {:error, :bad_directory, list}
+    end
+  end
+
+  defp get_build_path(mode \\ Mix.env()) do
+    Path.join(MishkaInstaller.get_config(:project_path) || File.cwd!(), ["_build/", "#{mode}/", "lib"])
   end
 end
