c0f74119cd17006b8c8f3e3ed3d929970d001a46
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-04-24 13:23:46 +0430

    add do_runtime_install to install a dep in runtime

--- lib/installer/run_time_sourcing.ex
+++ lib/installer/run_time_sourcing.ex
@@ -1,12 +1,16 @@
 defmodule MishkaInstaller.Installer.RunTimeSourcing do
-  def get_installed_apps() do
+  @type ensure() :: :sure_all_started | :load
 
+  @spec do_runtime_install(atom) :: {:ok, :application_ensure} | {:error, :application_ensure, ensure(), any}
+  def do_runtime_install(app) do
+    get_build_path()
+    |> File.ls!()
+    |> compare_dependencies()
+    |> prepend_compiled_apps()
+    application_ensure(app)
   end
 
-  def compiled_apps(_mode \\ Mix.env()) do
-
-  end
-
+  @spec compare_dependencies([tuple()], [String.t()]) :: [String.t()]
   def compare_dependencies(installed_apps \\ Application.loaded_applications, files_list) do
     installed_apps = Map.new(installed_apps, fn {app, _des, _ver} = item -> {Atom.to_string(app), item} end)
     Enum.map(files_list, fn app_name ->
@@ -20,6 +24,7 @@ defmodule MishkaInstaller.Installer.RunTimeSourcing do
     |> Enum.reject(& is_nil(&1))
   end
 
+  @spec deps :: list
   def deps() do
     [{:get, "deps.get"}, {:compile, "deps.compile"}]
     |> Enum.map(fn {operation, command} ->
@@ -28,17 +33,29 @@ defmodule MishkaInstaller.Installer.RunTimeSourcing do
     end)
   end
 
+  def prepend_compiled_apps([]), do: {:error, :prepend_compiled_apps, :no_directory}
   def prepend_compiled_apps(files_list) do
     files_list
     |> Enum.map(& {String.to_atom(&1), Path.join(get_build_path() <> "/" <> &1, "ebin")  |> Code.prepend_path})
     |> Enum.filter(fn {_app, status} -> status == {:error, :bad_directory} end)
     |> case do
       [] -> {:ok, :prepend_compiled_apps}
-      list -> {:error, :bad_directory, list}
+      list -> {:error, :prepend_compiled_apps, :bad_directory, list}
     end
   end
 
   defp get_build_path(mode \\ Mix.env()) do
     Path.join(MishkaInstaller.get_config(:project_path) || File.cwd!(), ["_build/", "#{mode}/", "lib"])
+  end
+
+  defp application_ensure(app) do
+    with {:load, :ok} <- {:load, Application.load(app)},
+         {:sure_all_started, {:ok, _apps}} <- {:sure_all_started, Application.ensure_all_started(app)} do
+
+        {:ok, :application_ensure}
+    else
+      {:load, {:error, term}} -> {:error, :application_ensure, :load, term}
+      {:sure_all_started, {:error, {app, term}}} -> {:error, :application_ensure, :sure_all_started, {app, term}}
+    end
   end
 end
