65647a2c199020353a0b33828bb1631f9135ac3c
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-05-01 17:39:19 +0430

    add CRUD for DepChangesProtector Genserver

--- lib/installer/dep_changes_protector.ex
+++ lib/installer/dep_changes_protector.ex
@@ -8,13 +8,64 @@ defmodule MishkaInstaller.Installer.DepChangesProtector do
   end
 
   def push(app, status) do
-    GenServer.cast(__MODULE__, {:push, app: app, status: status})
+    GenServer.call(__MODULE__, {:push, app: app, status: status})
+  end
+
+  def get(app) do
+    GenServer.call(__MODULE__, {:get, app})
+  end
+
+  def get() do
+    GenServer.call(__MODULE__, :get)
+  end
+
+  def pop(app) do
+    GenServer.call(__MODULE__, {:pop, app})
+  end
+
+  def clean() do
+    GenServer.cast(__MODULE__, :clean)
   end
 
   @impl true
   def init(stack) do
     Logger.info("OTP Dependencies changes protector Cache server was started")
     {:ok, stack}
+  end
+
+  @impl true
+  def handle_call({:push, app: app, status: status}, _from, state) do
+    new_state =
+      case Enum.find(state, & &1.app == app) do
+        nil ->
+          new_app = %{app: app, status: status, time: DateTime.utc_now}
+          {:reply, new_app, state ++ [new_app]}
+        _ ->
+
+          {:reply, {:duplicate, app}, state}
+      end
+    new_state
+  end
+
+  @impl true
+  def handle_call({:get, app}, _from, state) do
+    {:reply, Enum.find(state, & &1.app == app), state}
+  end
+
+  @impl true
+  def handle_call(:get, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_call({:pop, app}, _from, state) do
+    new_state = Enum.reject(state, & &1.app == app)
+    {:reply, new_state, new_state}
+  end
+
+  @impl true
+  def handle_cast(:clean, _state) do
+    {:noreply, []}
   end
 
   def check_josn_file_exist?() do
