5db55307daed64ef98f71871650b21da2fe1033d
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-05-02 14:00:21 +0430

    seperate json_check_and_create into DepChangesProtector

--- lib/installer/dep_changes_protector.ex
+++ lib/installer/dep_changes_protector.ex
@@ -1,6 +1,7 @@
 defmodule MishkaInstaller.Installer.DepChangesProtector do
   use GenServer
   require Logger
+  @re_check_json_time 10_000
 
   @spec start_link(list()) :: :ignore | {:error, any} | {:ok, pid}
   def start_link(args \\ []) do
@@ -35,23 +36,15 @@ defmodule MishkaInstaller.Installer.DepChangesProtector do
 
   @impl true
   def handle_continue(:check_json, state) do
-    Process.send_after(self(), :check_json, 10_000)
+    Process.send_after(self(), :check_json, @re_check_json_time)
     {:noreply,state}
   end
 
   @impl true
   def handle_info(:check_json, _state) do
     Logger.info("OTP Dependencies changes protector Cache server was valued by JSON.")
-    new_state =
-      with {:ok, :check_or_create_deps_json, _json} <- MishkaInstaller.Installer.DepHandler.check_or_create_deps_json(),
-           {:ok, :read_dep_json, data} <- MishkaInstaller.Installer.DepHandler.read_dep_json() do
-            data
-            |> Enum.filter(&(&1["dependency_type"] == "soft_update"))
-            |> Enum.map(&(%{app: &1["app"], status: &1["dependency_type"], time: DateTime.utc_now}))
-      else
-        _ -> []
-      end
-    {:noreply, new_state}
+    Process.send_after(self(), :check_json, @re_check_json_time)
+    {:noreply, json_check_and_create()}
   end
 
   @impl true
@@ -89,15 +82,24 @@ defmodule MishkaInstaller.Installer.DepChangesProtector do
     {:noreply, []}
   end
 
-  def check_josn_file_exist?() do
-    # TODO: Check if extension json file does not exist, create it with database
-  end
-
   def is_there_update?() do
     # TODO: Check is there update from a developer json url, and get it from plugin/componnet mix file, Consider queue
   end
 
   def is_dependency_compiling?() do
     # TODO: Create queue for installing multi deps, and compiling, check oban: https://github.com/sorentwo/oban
+  end
+
+  defp json_check_and_create() do
+    with {:ok, :check_or_create_deps_json, _json} <- MishkaInstaller.Installer.DepHandler.check_or_create_deps_json(),
+           {:ok, :read_dep_json, data} <- MishkaInstaller.Installer.DepHandler.read_dep_json() do
+            data
+            |> Enum.filter(&(&1["dependency_type"] == "soft_update"))
+            |> Enum.map(&(%{app: &1["app"], status: &1["dependency_type"], time: DateTime.utc_now}))
+      else
+        _ ->
+          # TODO: send an activity as error log
+          []
+      end
   end
 end
