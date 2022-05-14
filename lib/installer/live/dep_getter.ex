45be72ac123e4d90d31d3d31b907da77867e375c
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-05-14 18:12:26 +0430

    add registered app html to let admin user decide what to do

--- lib/installer/live/dep_getter.ex
+++ lib/installer/live/dep_getter.ex
@@ -1,5 +1,6 @@
 defmodule MishkaInstaller.Installer.Live.DepGetter do
   use Phoenix.LiveView
+  use DvoteWeb, :live_view
   alias Phoenix.LiveView.JS
   alias MishkaInstaller.Installer.DepHandler
 
@@ -19,6 +20,7 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
     socket =
       socket
       |> assign(:selected_form, :upload)
+      |> assign(:app_name, nil)
       |> assign(:status_message, {nil, nil})
       |> assign(:uploaded_files, [])
       |> allow_upload(:dep, accept: ~w(.jpg .jpeg .png), max_entries: 1)
@@ -51,8 +53,19 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
   def handle_event("form_select", %{"type" => type} = _params, socket) when type in ["upload", "hex", "git"] do
     socket =
       socket
-      |> assign(:status_message, {nil, nil})
+      |> assign(:status_message, {:nil, nil})
+      |> assign(:app_name, nil)
       |> assign(:selected_form, String.to_atom(type))
+    {:noreply, socket}
+  end
+
+  @impl Phoenix.LiveView
+  def handle_event("update_app", %{"type" => type} = _params, socket) when type in ["force_update", "soft_update"] do
+    socket =
+      socket
+      |> assign(:status_message, {:info, "Your request was sent, after receiving any changes we send you a notification"})
+      |> assign(:app_name, nil)
+      |> assign(:selected_form, :upload)
     {:noreply, socket}
   end
 
@@ -76,9 +89,9 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
         {:ok, :no_state, msg} ->
           socket
           |> assign(:status_message, {:success, msg})
-        {:ok, :registered_app, msg} ->
-          # TODO: create html with a tag to ask user force update or not
+        {:ok, :registered_app, msg, app_name} ->
           socket
+          |> assign(:app_name, app_name)
           |> assign(:status_message, {:info, msg})
         {:error, msg} ->
           socket
@@ -108,7 +121,7 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
           MishkaInstaller.Dependency.update_app_version(app["app"], pkg["latest_stable_version"])
           case DepHandler.dependency_changes_notifier(pkg["name"]) do
             {:ok, :dependency_changes_notifier, :no_state, msg} -> {:ok, :no_state, msg}
-            {:ok, :dependency_changes_notifier, :registered_app, msg} -> {:ok, :registered_app, msg}
+            {:ok, :dependency_changes_notifier, :registered_app, msg} -> {:ok, :registered_app, msg, pkg["name"]}
             {:error, :dependency_changes_notifier, msg} -> {:error, msg}
           end
         end
@@ -186,60 +199,92 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
   defp dep_form(:hex, assigns) do
     ~H"""
       <section id="dep-hex-getter" class="col-md-6 mx-auto">
-      <form id="extensions-upload-form" phx-submit="save" phx-change="validate">
-        <% {status, message} = @status_message %>
-        <%= if !is_nil(message) do %>
-          <div class="container" id="dep-status-msg">
-            <div class={"alert alert-#{status}"} role="alert" phx-click={JS.hide(to: "#dep-status-msg")}><%= message %></div>
-            <div class="container h-25 d-inline-block"></div>
-          </div>
-        <% end %>
-        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-hexagon mb-4" viewBox="0 0 16 16">
-          <path d="M14 4.577v6.846L8 15l-6-3.577V4.577L8 1l6 3.577zM8.5.134a1 1 0 0 0-1 0l-6 3.577a1 1 0 0 0-.5.866v6.846a1 1 0 0 0 .5.866l6 3.577a1 1 0 0 0 1 0l6-3.577a1 1 0 0 0 .5-.866V4.577a1 1 0 0 0-.5-.866L8.5.134z"/>
-        </svg>
-        <div class="container h-25 d-inline-block"></div>
-        <input name="app" class="form-control form-control-lg mb-3 w-50 p-3 mx-auto" type="text" placeholder="Input app name like: mishka_installer" required>
-        <input type="hidden" id="hidden_type" name="select_form" value="hex">
-        <button type="submit" class="btn btn-outline-secondary mt-4 mb-4">Download/Update from hex</button>
-        <div class="container h-25 d-inline-block"></div>
-        <span class="mt-4 mb-4">
-          <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="upload">Upload</a> - <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="git">Or get from Git</a>
-        </span>
-      </form>
+        <form id="extensions-upload-form" phx-submit="save" phx-change="validate">
+          <% {status, message} = @status_message %>
+          <%= if !is_nil(message) do %>
+            <div class="container" id="dep-status-msg">
+              <div class={"alert alert-#{status}"} role="alert" phx-click={JS.hide(to: "#dep-status-msg")}><%= message %></div>
+              <div class="container h-25 d-inline-block"></div>
+            </div>
+          <% end %>
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-hexagon mb-4" viewBox="0 0 16 16">
+            <path d="M14 4.577v6.846L8 15l-6-3.577V4.577L8 1l6 3.577zM8.5.134a1 1 0 0 0-1 0l-6 3.577a1 1 0 0 0-.5.866v6.846a1 1 0 0 0 .5.866l6 3.577a1 1 0 0 0 1 0l6-3.577a1 1 0 0 0 .5-.866V4.577a1 1 0 0 0-.5-.866L8.5.134z"/>
+          </svg>
+          <div class="container h-25 d-inline-block"></div>
+          <input name="app" class="form-control form-control-lg mb-3 w-50 p-3 mx-auto" type="text" placeholder="Input app name like: mishka_installer" required>
+          <input type="hidden" id="hidden_type" name="select_form" value="hex">
+          <button type="submit" class="btn btn-outline-secondary mt-4 mb-4">Download/Update from hex</button>
+          <div class="container h-25 d-inline-block"></div>
+          <span class="mt-4 mb-4">
+            <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="upload">Upload</a> - <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="git">Or get from Git</a>
+          </span>
+        </form>
       </section>
     """
   end
 
   defp dep_form(:git, assigns) do
     ~H"""
+      <section id="dep-git-getter" class="col-md-6 mx-auto">
+        <form id="extensions-upload-form" phx-submit="save" phx-change="validate">
+          <% {status, message} = @status_message %>
+          <%= if !is_nil(message) do %>
+            <div class="container" id="dep-status-msg">
+              <div class={"alert alert-#{status}"} role="alert" phx-click={JS.hide(to: "#dep-status-msg")}><%= message %></div>
+              <div class="container h-25 d-inline-block"></div>
+            </div>
+          <% end %>
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-git mb-4" viewBox="0 0 16 16">
+            <path d="M15.698 7.287 8.712.302a1.03 1.03 0 0 0-1.457 0l-1.45 1.45 1.84 1.84a1.223 1.223 0 0 1 1.55 1.56l1.773 1.774a1.224 1.224 0 0 1 1.267 2.025 1.226 1.226 0 0 1-2.002-1.334L8.58 5.963v4.353a1.226 1.226 0 1 1-1.008-.036V5.887a1.226 1.226 0 0 1-.666-1.608L5.093 2.465l-4.79 4.79a1.03 1.03 0 0 0 0 1.457l6.986 6.986a1.03 1.03 0 0 0 1.457 0l6.953-6.953a1.031 1.031 0 0 0 0-1.457"/>
+          </svg>
+          <div class="container h-25 d-inline-block"></div>
+          <input name="app" class="form-control form-control-lg mb-3" type="text" placeholder="App name" required>
+          <div class="container h-25 d-inline-block"></div>
+          <input name="url" class="form-control form-control-lg mb-3" type="text" placeholder="Your Git url" required>
+          <div class="container h-25 d-inline-block"></div>
+          <input name="git_tag" class="form-control form-control-lg mb-3" type="text" placeholder="Git tag, Ex: 0.0.2 or master">
+          <div class="container h-25 d-inline-block"></div>
+          <input name="update_server" class="form-control form-control-lg mb-3" type="text" placeholder="Update server, Ex: https://your_url.com/server.json">
+          <div class="container h-25 d-inline-block"></div>
+          <input type="hidden" id="hidden_type" name="select_form" value="git">
+          <button type="submit" class="btn btn-outline-secondary mt-4 mb-4">Download/Update from Git</button>
+          <div class="container h-25 d-inline-block"></div>
+          <span class="mt-4 mb-4">
+            <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="upload">Upload</a> - <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="hex">Or get from hex</a>
+          </span>
+        </form>
+      </section>
+    """
+  end
+
+  defp dep_form(:registered_app, assigns) do
+    ~H"""
       <section id="dep-hex-getter" class="col-md-6 mx-auto">
-      <form id="extensions-upload-form" phx-submit="save" phx-change="validate">
-        <% {status, message} = @status_message %>
-        <%= if !is_nil(message) do %>
-          <div class="container" id="dep-status-msg">
-            <div class={"alert alert-#{status}"} role="alert" phx-click={JS.hide(to: "#dep-status-msg")}><%= message %></div>
-            <div class="container h-25 d-inline-block"></div>
+        <form id="extensions-upload-form" phx-submit="save" phx-change="validate">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-exclamation-triangle mb-4" viewBox="0 0 16 16">
+            <path d="M7.938 2.016A.13.13 0 0 1 8.002 2a.13.13 0 0 1 .063.016.146.146 0 0 1 .054.057l6.857 11.667c.036.06.035.124.002.183a.163.163 0 0 1-.054.06.116.116 0 0 1-.066.017H1.146a.115.115 0 0 1-.066-.017.163.163 0 0 1-.054-.06.176.176 0 0 1 .002-.183L7.884 2.073a.147.147 0 0 1 .054-.057zm1.044-.45a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566z"/>
+            <path d="M7.002 12a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 5.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995z"/>
+          </svg>
+          <% {status, message} = @status_message %>
+          <%= if !is_nil(message) do %>
+            <div class="container" id="dep-status-msg">
+              <div class="container h-25 d-inline-block"></div>
+              <div class={"alert alert-#{status}"} role="alert" phx-click={JS.hide(to: "#dep-status-msg")}><%= message %></div>
+            </div>
+          <% end %>
+          <div class="alert alert-secondary text-center prefer-alert">
+            We really recommend you to <b>wait for the app concerned response</b>, because maybe there is an important state that you need after updating!!
           </div>
-        <% end %>
-        <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-git mb-4" viewBox="0 0 16 16">
-          <path d="M15.698 7.287 8.712.302a1.03 1.03 0 0 0-1.457 0l-1.45 1.45 1.84 1.84a1.223 1.223 0 0 1 1.55 1.56l1.773 1.774a1.224 1.224 0 0 1 1.267 2.025 1.226 1.226 0 0 1-2.002-1.334L8.58 5.963v4.353a1.226 1.226 0 1 1-1.008-.036V5.887a1.226 1.226 0 0 1-.666-1.608L5.093 2.465l-4.79 4.79a1.03 1.03 0 0 0 0 1.457l6.986 6.986a1.03 1.03 0 0 0 1.457 0l6.953-6.953a1.031 1.031 0 0 0 0-1.457"/>
-        </svg>
-        <div class="container h-25 d-inline-block"></div>
-        <input name="app" class="form-control form-control-lg mb-3" type="text" placeholder="App name" required>
-        <div class="container h-25 d-inline-block"></div>
-        <input name="url" class="form-control form-control-lg mb-3" type="text" placeholder="Your Git url" required>
-        <div class="container h-25 d-inline-block"></div>
-        <input name="git_tag" class="form-control form-control-lg mb-3" type="text" placeholder="Git tag, Ex: 0.0.2 or master">
-        <div class="container h-25 d-inline-block"></div>
-        <input name="update_server" class="form-control form-control-lg mb-3" type="text" placeholder="Update server, Ex: https://your_url.com/server.json">
-        <div class="container h-25 d-inline-block"></div>
-        <input type="hidden" id="hidden_type" name="select_form" value="git">
-        <button type="submit" class="btn btn-outline-secondary mt-4 mb-4">Download/Update from Git</button>
-        <div class="container h-25 d-inline-block"></div>
-        <span class="mt-4 mb-4">
-          <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="upload">Upload</a> - <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="hex">Or get from hex</a>
-        </span>
-      </form>
+          <div class="container h-25 d-inline-block"></div>
+          <button type="button" class="btn btn-outline-success" phx-click="update_app" phx-value-type="soft_update">
+            <span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+            Wait for the app response!
+          </button>
+          <button type="button" class="btn btn-outline-danger" phx-click="update_app" phx-value-type="force_update">
+
+           Do Force Update now!
+          </button>
+        </form>
       </section>
     """
   end
