31892dfb19947bfd0ba22c4c59a55f6bda1d49c4
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-06-18 18:43:52 +0430

    add installed_plugins html without showing updates of plugins

--- lib/installer/live/dep_getter.ex
+++ lib/installer/live/dep_getter.ex
@@ -308,6 +308,40 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
     </section>
     """
   end
+
+  defp dep_form(:installed_plugins, assigns) do
+    ~H"""
+    <section id="dep-getter" class="col-md-6 mx-auto">
+      <div class="bd-callout bd-callout-info mb-5">
+        <h3 class="mb-3" id="using-embedded-svg">Installed Apps</h3>
+        <p>
+          It should be noted the <span class="text-primary">blue</span> apps exist in JSON file, and it is added by administrator and another apps exist in core, or they are sub-dependencies.
+          Each time a new update comes in for one of the plugins, the <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-down text-warning" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708l2 2z"/><path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/></svg> image comes next to it
+        </p>
+      </div>
+      <hr class="mb-5 mt-5">
+      <%= for {app, _des, ver} <- Application.loaded_applications do %>
+        <%= if app in Enum.map(DepHandler.compare_dependencies_with_json(), & &1.app) do %>
+          <small id={Ecto.UUID.generate} class="d-inline-flex mb-3 px-2 py-1 fw-semibold text-primary bg-primary bg-opacity-10 border border-success border-opacity-10 rounded-2">
+            <%= "#{app}" %> in v<%= "#{ver}" %> &nbsp;
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-arrow-down" viewBox="0 0 16 16">
+              <path fill-rule="evenodd" d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708l2 2z"/>
+              <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+            </svg>
+          </small>
+        <% else %>
+          <small id={Ecto.UUID.generate} class="d-inline-flex mb-3 px-2 py-1 fw-semibold text-success bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2">
+            <%= "#{app}" %> in v<%= "#{ver}" %>
+          </small>
+        <% end %>
+      <% end %>
+      <p class="mt-4 mb-4 text-center">
+        <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="upload">Upload</a> - <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="hex">Get from Hex</a> - <a class="dep-link text-decoration-none" phx-click="form_select" phx-value-type="git">Or from Git</a>
+      </p>
+    </section>
+    """
+  end
+
   defp icon(assigns) do
     ~H"""
     <p id="dep-icon-getter" class="col-md-6 mx-auto text-end">
