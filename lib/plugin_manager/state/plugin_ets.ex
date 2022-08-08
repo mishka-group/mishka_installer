2efdc7290aecc4722214b54c7060fc1cc0902b99
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-08-09 01:01:07 +0430

    add plugin ets doc, based on #67

--- lib/plugin_manager/state/plugin_ets.ex
+++ lib/plugin_manager/state/plugin_ets.ex
@@ -1,41 +1,6 @@
 defmodule MishkaInstaller.PluginETS do
   @moduledoc """
-    ETS part to optimization state instead of dynamic supervisor and Genserver
-
-    1. `public` — Read/Write available to all processes.
-    2. `protected` — Read available to all processes. Only writable by owner process. This is the default.
-    3. `private` — Read/Write limited to owner process.
-
-    #### Essential functions in ETS
-
-    1.  :ets.new
-    ```elixir
-    :ets.new(@tab, [:set, :named_table, :public, read_concurrency: true, write_concurrency: true])
-    ```
-    2.  :ets.insert
-    3.  :ets.insert_new
-    4.  :ets.lookup
-    5.  :ets.match
-    6.  :ets.match_object
-    7.  :ets.select
-    8.  :ets.fun2ms
-    9.  :ets.delete
-    10. :dets.open_file
-    11. :ets.update_counter
-    12. :ets.delete_all_objects
-    13. :ets.tab2list - to spy on the data in the table
-    14. :ets.update_counter
-    15. :ets.all
-    16. :ets.info
-
-    ### Reference
-
-    * https://www.erlang.org/doc/man/ets.html
-    * https://dockyard.com/blog/2017/05/19/optimizing-elixir-and-phoenix-with-ets
-    * https://learnyousomeerlang.com/ets
-    * https://elixir-lang.org/getting-started/mix-otp/ets.html
-    * https://elixirschool.com/en/lessons/storage/ets
-    * https://github.com/TheFirstAvenger/ets
+  This module provides a series of essential functions for storing plugins on ETS.
   """
   use GenServer
   require Logger
@@ -46,6 +11,7 @@ defmodule MishkaInstaller.PluginETS do
     GenServer.start_link(__MODULE__, args, name: __MODULE__)
   end
 
+  @doc false
   def child_spec(process_name) do
     %{
       id: __MODULE__,
@@ -85,10 +51,16 @@ defmodule MishkaInstaller.PluginETS do
     {:noreply, state}
   end
 
+  @doc """
+  Storing a plug-in on the ETS table.
+  """
   def push(%MishkaInstaller.PluginState{name: name, event: event} = state) do
     ETS.Set.put!(table(), {String.to_atom(name), event, state})
   end
 
+  @doc """
+  Getting a plug-in from the ETS table.
+  """
   def get(module: name) do
     case ETS.Set.get(table(), String.to_atom(name)) do
       {:ok, {_key, _event, data}} -> data
@@ -96,11 +68,18 @@ defmodule MishkaInstaller.PluginETS do
     end
   end
 
+  @doc """
+  Getting all plug-ins from the ETS table.
+  """
   def get_all(event: event_name) do
     ETS.Set.match!(table(), {:_, event_name, :"$3"})
     |> Enum.map(&List.first/1)
   end
 
+  @doc """
+   - Deleting a plug-in from the ETS table.
+   - Deleting plugins from the ETS table based on a specific event.
+  """
   def delete(module: module_name) do
     ETS.Set.delete(table(), String.to_atom(module_name))
   end
@@ -120,6 +99,9 @@ defmodule MishkaInstaller.PluginETS do
     end
   end
 
+  @doc """
+  Initializing ETS table with database
+  """
   def sync_with_database() do
     MishkaInstaller.Plugin.plugins()
     |> Enum.reject(&(&1.status in [:stopped]))
