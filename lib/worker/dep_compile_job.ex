37d0e9fa5022c2c92e85d78053b0a71593cc0d25
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-08-13 14:11:11 +0430

    add DepCompileJob docs, fix #70

--- lib/worker/dep_compile_job.ex
+++ lib/worker/dep_compile_job.ex
@@ -1,11 +1,14 @@
 defmodule MishkaInstaller.DepCompileJob do
+  @moduledoc """
+  With the assistance of this module, you will be able to construct a queue to process and install extensions.
+
+  This module's responsibility includes reactivating the queue using the `MishkaInstaller.Installer.DepChangesProtector` module as one of its tasks.
+  """
   use Oban.Worker, queue: :compile_events, max_attempts: 1
   alias MishkaInstaller.Installer.DepHandler
   require Logger
 
-  # TODO: check a ref exists or not and after that do compile, if is there so wait
-  # TODO: MishkaInstaller.Installer.DepChangesProtector.is_dependency_compiling?()
-
+  @doc false
   @impl Oban.Worker
   def perform(%Oban.Job{args: %{"app" => app, "type" => type}}) when is_atom(type),
     do: run_compile(app, type)
@@ -15,6 +18,20 @@ defmodule MishkaInstaller.DepCompileJob do
 
   def perform(%Oban.Job{args: %{"app" => app, "type" => _type}}), do: run_compile(app, :cmd)
 
+  @doc """
+  Register an extension to the compiling queue.
+  With the assistance of this function, you will be able to construct a queue,
+  download and upload plugins in a sequential fashion from a variety of sources, and register them in your system.
+
+
+  ## Examples
+
+  ```elixir
+  MishkaInstaller.DepCompileJob.add_job("mishka_installer", :cmd)
+  # or
+  MishkaInstaller.DepCompileJob.add_job("mishka_installer", :port)
+  ```
+  """
   @spec add_job(String.t(), atom()) :: {:error, any} | {:ok, Oban.Job.t()}
   def add_job(app, type) do
     %{app: app, type: type}
