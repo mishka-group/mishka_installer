f37ff9617c0f982021b4fe4401e311ef13832e48
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-08-17 00:51:58 +0430

    change type to callback, based on #73

--- lib/plugin_manager/event/reference/on_change_dependency.ex
+++ lib/plugin_manager/event/reference/on_change_dependency.ex
@@ -14,19 +14,19 @@ defmodule MishkaInstaller.Reference.OnChangeDependency do
   @type t :: state()
   @type optional_callbacks :: {:ok, ref(), registerd_info()} | {:error, ref(), reason()}
 
-  @doc "This type can be used when you want to register a plugin"
+  @doc "This Callback can be used when you want to register a plugin"
   @callback initial(list()) :: {:ok, ref(), list()} | {:error, ref(), reason()}
-  @doc "This type can be used when you want to call a plugin"
+  @doc "This Callback can be used when you want to call a plugin"
   @callback call(state()) :: {:reply, state()} | {:reply, :halt, state()}
-  @doc "This type can be used when you want to stop a plugin"
+  @doc "This Callback can be used when you want to stop a plugin"
   @callback stop(registerd_info()) :: optional_callbacks()
-  @doc "This type can be used when you want to restart a plugin"
+  @doc "This Callback can be used when you want to restart a plugin"
   @callback restart(registerd_info()) :: optional_callbacks()
-  @doc "This type can be used when you want to start a plugin"
+  @doc "This Callback can be used when you want to start a plugin"
   @callback start(registerd_info()) :: optional_callbacks()
-  @doc "This type can be used when you want to delete a plugin"
+  @doc "This Callback can be used when you want to delete a plugin"
   @callback delete(registerd_info()) :: optional_callbacks()
-  @doc "This type can be used when you want to unregister a plugin"
+  @doc "This Callback can be used when you want to unregister a plugin"
   @callback unregister(registerd_info()) :: optional_callbacks()
   # Developer can use this callbacks if he/she needs
   @optional_callbacks stop: 1, restart: 1, start: 1, delete: 1, unregister: 1
