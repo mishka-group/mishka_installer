bcfb684a9274cfd9d2ae0a5882766441be7c002c
shahryar tavakkoli <shahryar.tbiz@gmail.com>
2022-10-23 16:32:48 +0330

    add description of each plugin as tooltip, fix #45

--- lib/installer/live/dep_getter.ex
+++ lib/installer/live/dep_getter.ex
@@ -755,9 +755,9 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
       </div>
       <hr class="mb-5 mt-5">
       <div id="all_loaded_app" class="container">
-        <%= for {app, _des, ver} <- @loaded_apps do %>
+        <%= for {app, des, ver} <- @loaded_apps do %>
           <%= if app in Enum.map(DepHandler.compare_dependencies_with_json(), & &1.app) do %>
-              <span id={"#{app}" <> "dropdown-class"} class="dropdown">
+              <span id={"#{app}" <> "dropdown-class"} class="dropdown" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title={des}>
                 <small id={app} class="d-inline-flex mb-2 px-2 py-1 fw-semibold text-primary bg-primary bg-opacity-10 border border-success border-opacity-10 rounded-2" type="button" data-bs-toggle="dropdown" aria-expanded="false">
                   <%= "#{app}" %> in v<%= "#{ver}" %> &nbsp;
                   <%= if app in @update_list do %>
@@ -780,7 +780,7 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
                 </ul>
               </span>
           <% else %>
-            <small id={app} class="d-inline-flex mx-1 mb-2 px-2 py-1 fw-semibold text-success bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2">
+            <small id={app} class="d-inline-flex mx-1 mb-2 px-2 py-1 fw-semibold text-success bg-success bg-opacity-10 border border-success border-opacity-10 rounded-2" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title={des}>
               <%= "#{app}" %> in v<%= "#{ver}" %>
             </small>
           <% end %>
@@ -849,7 +849,7 @@ defmodule MishkaInstaller.Installer.Live.DepGetter do
           <path fill-rule="evenodd" d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2Z"/>
         </svg>
       </span>
-      <span phx-click="form_select" phx-value-type="installed_plugins">
+      <span id="plugin-icon" phx-click="form_select" phx-value-type="installed_plugins" phx-hook="EnableToolTip">
         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-plug" viewBox="0 0 16 16">
           <path d="M6 0a.5.5 0 0 1 .5.5V3h3V.5a.5.5 0 0 1 1 0V3h1a.5.5 0 0 1 .5.5v3A3.5 3.5 0 0 1 8.5 10c-.002.434-.01.845-.04 1.22-.041.514-.126 1.003-.317 1.424a2.083 2.083 0 0 1-.97 1.028C6.725 13.9 6.169 14 5.5 14c-.998 0-1.61.33-1.974.718A1.922 1.922 0 0 0 3 16H2c0-.616.232-1.367.797-1.968C3.374 13.42 4.261 13 5.5 13c.581 0 .962-.088 1.218-.219.241-.123.4-.3.514-.55.121-.266.193-.621.23-1.09.027-.34.035-.718.037-1.141A3.5 3.5 0 0 1 4 6.5v-3a.5.5 0 0 1 .5-.5h1V.5A.5.5 0 0 1 6 0zM5 4v2.5A2.5 2.5 0 0 0 7.5 9h1A2.5 2.5 0 0 0 11 6.5V4H5z"/>
         </svg>
