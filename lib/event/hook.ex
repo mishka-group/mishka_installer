7f44ce540bf6e338cd79eaa4a060feefc190e778
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-06-15 11:58:27 +0330

    VIP - add hooks functions

--- lib/event/hook.ex
+++ lib/event/hook.ex
@@ -5,7 +5,8 @@ defmodule MishkaInstaller.Event.Hook do
   defmacro __using__(opts \\ []) do
     quote bind_quoted: [opts: opts] do
       use GenServer, restart: :transient
-      alias MishkaInstaller.Event.{Event, Hook}
+      alias MishkaInstaller.Event.{Event, Hook, EventHandler}
+      alias MishkaInstaller.Event.ModuleStateCompiler, as: MSE
 
       # Based on https://elixirforum.com/t/59168/5
       @app_config Mix.Project.config()
@@ -73,7 +74,23 @@ defmodule MishkaInstaller.Event.Hook do
         new_state =
           Keyword.merge(state, name: __MODULE__, event: @plugin_event, status: :starting)
 
-        {:ok, new_state}
+        {:ok, new_state, {:continue, :start_plugin}}
+      end
+
+      @impl true
+      def handle_continue(:start_plugin, state) do
+        MnesiaAssistant.Table.wait_for_tables([Event], @wait_for_tables)
+
+        new_state =
+          if :persistent_term.get(:event_status, nil) == "ready" do
+            Hook.register_start_helper(__MODULE__, state)
+          else
+            Process.send_after(__MODULE__, :register_start_again, 1000)
+            state
+          end
+
+        Process.send_after(__MODULE__, :status, 1000)
+        {:noreply, new_state}
       end
 
       @impl true
@@ -87,6 +104,74 @@ defmodule MishkaInstaller.Event.Hook do
       end
 
       @impl true
+      def handle_info(:start_again, state) do
+        event = Keyword.get(state, :event)
+        db_plg = Event.get(:name, Keyword.get(state, :name))
+        module = MSE.module_event_name(event)
+
+        new_state =
+          if MSE.safe_initialize?(event) and module.is_initialized?(db_plg) do
+            Keyword.merge(state, status: db_plg.status, depends: db_plg.depends)
+          else
+            Hook.start_helper(__MODULE__, state, db_plg)
+          end
+
+        {:noreply, new_state}
+      end
+
+      def handle_info(%{status: status, data: data}, state)
+          when status in [:start, :stop, :unregister] do
+        event = Keyword.get(state, :event)
+        depends = Keyword.get(state, :depends, [])
+        # We need some state, it will be saved again or not, it should not be loaded if
+        # |__ it is restored
+        event_status = :persistent_term.get(:event_status, nil)
+
+        new_state =
+          with true <- event_status == "ready",
+               true <- event == Map.get(data, :event),
+               true <- Map.get(data, :name) in depends,
+               :ok <- Event.allowed_events?(depends),
+               {:ok, struct} <- Event.write(:name, @plugin_name, %{status: :restarted}),
+               _ok <- EventHandler.do_compile(struct.event, :re_event) do
+            Keyword.merge(state, status: :restarted)
+          else
+            _ -> state
+          end
+
+        {:noreply, new_state}
+      end
+
+      def handle_info(%{status: :re_event, data: _data}, state) do
+        new_state =
+          case Event.get(:name, state[:name]) do
+            nil -> state
+            data -> Keyword.merge(state, status: data.status)
+          end
+
+        {:noreply, state}
+      end
+
+      def handle_info(:status, state) do
+        Process.send_after(__MODULE__, :status, @checking)
+
+        new_state =
+          with "ready" <- :persistent_term.get(:event_status, nil),
+               {:module, module} <- Code.ensure_loaded(MSE.module_event_name(@plugin_event)),
+               data when not is_nil(data) <-
+                 Enum.find(module.initialize().plugins, &(&1.name == @plugin_name)) do
+            case Event.get(:name, @plugin_name) do
+              nil -> state
+              plugin -> Keyword.merge(state, status: plugin.status)
+            end
+          else
+            _ -> state
+          end
+
+        {:noreply, new_state}
+      end
+
+      @impl true
       def handle_info(_reason, state) do
         {:noreply, state}
       end
@@ -96,7 +181,7 @@ defmodule MishkaInstaller.Event.Hook do
   ####################################################################################
   ######################### (▰˘◡˘▰) Functions (▰˘◡˘▰) ##########################
   ####################################################################################
-  @spec call(String.t(), any(), any()) :: any()
+  @spec call(String.t(), any(), keyword()) :: any()
   def call(event, data, args \\ []) do
     module = ModuleStateCompiler.module_event_name(event)
 
@@ -110,4 +195,31 @@ defmodule MishkaInstaller.Event.Hook do
   ####################################################################################
   ########################## (▰˘◡˘▰) Helper (▰˘◡˘▰) ############################
   ####################################################################################
+  @spec start_helper(module(), keyword(), any()) :: keyword()
+  @doc false
+  def start_helper(module, state, reg_db_plg) do
+    case module.start() do
+      {:ok, st_db_plg} ->
+        Keyword.merge(state, status: st_db_plg.status, depends: st_db_plg.depends)
+
+      {:error, [%{field: :event, action: :compile}]} ->
+        Process.send_after(module, :start_again, 1000)
+        Keyword.merge(state, status: :held, depends: reg_db_plg.depends)
+
+      _error ->
+        Keyword.merge(state, status: reg_db_plg.status, depends: reg_db_plg.depends)
+    end
+  end
+
+  @doc false
+  @spec register_start_helper(module(), keyword()) :: keyword()
+  def register_start_helper(module, state) do
+    case module.register() do
+      {:ok, reg_db_plg} ->
+        start_helper(module, state, reg_db_plg)
+
+      _error ->
+        state
+    end
+  end
 end
