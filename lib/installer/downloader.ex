71ca938a4b553aefe27046b4b791c7c5b93eb98b
Shahryar Tavakkoli <shahryar.tbiz@gmail.com>
2024-06-19 11:18:47 +0330

    VIP - add downloader from hex, github

--- lib/installer/downloader.ex
+++ lib/installer/downloader.ex
@@ -19,6 +19,7 @@ defmodule MishkaInstaller.Installer.Downloader do
           | %{path: String.t()}
           | %{path: String.t(), release: String.t()}
           | %{path: String.t(), tag: String.t()}
+          | %{path: String.t(), branch: String.t() | tuple()}
 
   @type error_return :: {:error, [%{action: atom(), field: atom(), message: String.t()}]}
 
@@ -27,6 +28,89 @@ defmodule MishkaInstaller.Installer.Downloader do
   ####################################################################################
   ######################### (▰˘◡˘▰) Functions (▰˘◡˘▰) ##########################
   ####################################################################################
+  @spec download(download_type, pkg) :: okey_return | error_return
+  def download(:hex, %{app: app, tag: tag_name}) do
+    case build_url("https://repo.hex.pm/tarballs/#{app}-#{tag_name}.tar") do
+      %Req.Response{status: 200, body: body} -> {:ok, body}
+      _ -> mix_global_err()
+    end
+  end
+
+  def download(:github, %{path: path, branch: {branch, git: true}}) do
+    case build_url("https://codeload.github.com/#{path}/legacy.tar.gz/refs/heads/#{branch}") do
+      %Req.Response{status: 200, body: body} -> {:ok, body}
+      _ -> mix_global_err()
+    end
+  end
+
+  def download(:github, %{path: path, branch: branch}) do
+    case build_url("https://github.com/#{path}/archive/refs/heads/#{branch}.tar.gz") do
+      %Req.Response{status: 200, body: body} -> {:ok, body}
+      _ -> mix_global_err()
+    end
+  end
+
+  def download(:github, %{path: path, release: release}) do
+    case build_url("https://github.com/#{path}/archive/refs/tags/#{release}.tar.gz") do
+      %Req.Response{status: 200, body: body} -> {:ok, body}
+      _ -> mix_global_err()
+    end
+  end
+
+  def download(:github, %{path: path, tag: tag}) do
+    case build_url("https://github.com/#{path}/archive/refs/tags/#{tag}.tar.gz") do
+      %Req.Response{status: 200, body: body} -> {:ok, body}
+      _ -> mix_global_err()
+    end
+  end
+
+  def download(:hex, %{app: app}) do
+    case build_url("#{@hex_path}/#{String.trim(app)}") do
+      %Req.Response{status: 200, body: %{"latest_stable_version" => tag_name}} ->
+        download(:hex, %{app: app, tag: tag_name})
+
+      _ ->
+        mix_global_err()
+    end
+  end
+
+  def download(:github, %{path: path}) do
+    case build_url("#{@github_api_path}/#{path}") do
+      %Req.Response{status: 200, body: %{"default_branch" => branch_name}} ->
+        download(:github, %{path: path, branch: branch_name})
+
+      _ ->
+        mix_global_err()
+    end
+  end
+
+  def download(:github_latest_release, %{path: path}) do
+    case build_url("#{@github_api_path}/#{String.trim(path)}/releases/latest") do
+      %Req.Response{status: 200, body: %{"tag_name" => tag_name}} ->
+        download(:github, %{path: path, release: tag_name})
+
+      _ ->
+        mix_global_err()
+    end
+  end
+
+  def download(:github_latest_tag, %{path: path}) do
+    case build_url("#{@github_api_path}/#{String.trim(path)}/tags") do
+      %Req.Response{status: 200, body: body} when body != [] ->
+        if is_nil(List.first(body)) do
+          mix_global_err("No tags found.")
+        else
+          download(:github, %{path: path, tag: List.first(body)["name"]})
+        end
+
+      _ ->
+        mix_global_err()
+    end
+  end
+
+  # ************************************************************
+  # ************************************************************
+  # ************************************************************
   @spec get_mix(download_type, pkg) :: okey_return | error_return
   def get_mix(:hex, %{app: app, tag: tag_name}) do
     get_mix(:url, %{path: "#{@hex_preview_path}/#{app}/#{tag_name}/mix.exs"})
@@ -76,7 +160,11 @@ defmodule MishkaInstaller.Installer.Downloader do
   def get_mix(:github_latest_tag, %{path: path}) do
     case build_url("#{@github_api_path}/#{String.trim(path)}/tags") do
       %Req.Response{status: 200, body: body} when body != [] ->
-        get_mix(:github_tag, %{path: path, tag: List.first(body)["name"]})
+        if is_nil(List.first(body)) do
+          mix_global_err("No tags found.")
+        else
+          get_mix(:github_tag, %{path: path, tag: List.first(body)["name"]})
+        end
 
       _ ->
         mix_global_err()
@@ -93,8 +181,8 @@ defmodule MishkaInstaller.Installer.Downloader do
   ####################################################################################
   ########################## (▰˘◡˘▰) Helper (▰˘◡˘▰) ############################
   ####################################################################################
-  defp mix_global_err() do
-    message = "There is a problem downloading the mix.exs file."
+  defp mix_global_err(msg \\ nil) do
+    message = msg || "There is a problem downloading the mix.exs file."
     {:error, [%{message: message, field: :path, action: :package}]}
   end
 
